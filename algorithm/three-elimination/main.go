9479fe225e9cbaa7d342654beb1060c16e566e75
20190046 <494608448@qq.com>
2019-05-14 11:51:18 +0800

    更新 三消算法

--- algorithm/three-elimination/main.go
+++ algorithm/three-elimination/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"strconv"
+	"strings"
 )
 
 type point struct {
@@ -36,6 +39,33 @@ func init() {
 }
 
 func main() {
+	for {
+
+		onTrigger()
+
+		var p1, p2 string
+		fmt.Println("请输入要移动的棋子的位置 与 移动后的位置:")
+		fmt.Scanln(&p1, &p2)
+
+		X1, _ := strconv.ParseFloat(strings.Split(p1, ",")[0], 64)
+		Y1, _ := strconv.ParseFloat(strings.Split(p1, ",")[1], 64)
+
+		X2, _ := strconv.ParseFloat(strings.Split(p2, ",")[0], 64)
+		Y2, _ := strconv.ParseFloat(strings.Split(p2, ",")[1], 64)
+
+		if math.Abs(X2-X1) > 1 || math.Abs(Y2-Y1) > 1 {
+			fmt.Println("请输入正确的位置")
+		} else {
+			chessboard[int(X1)][int(Y1)], chessboard[int(X2)][int(Y2)] = chessboard[int(X2)][int(Y2)], chessboard[int(X1)][int(Y1)]
+			for c := range chessboard {
+				fmt.Println(chessboard[c])
+			}
+		}
+	}
+}
+
+func onTrigger() {
+
 	for row, cb := range chessboard {
 		for col := range cb {
 			p := point{row, col}
@@ -43,9 +73,35 @@ func main() {
 		}
 	}
 
+	initChessboard(chessboard)
+
 	for c := range chessboard {
 		fmt.Println(chessboard[c])
 	}
+
+}
+
+// ScanPos is 扫描棋盘
+func ScanPos(p *point) bool {
+	var bol bool
+	if p.X < len(chessboard)-2 {
+		a, b, c := chessboard[p.X+1][p.Y], chessboard[p.X+2][p.Y], chessboard[p.X][p.Y]
+		if a == b && b == c {
+			bol = true
+			fmt.Println("X消除 ------")
+			chessboard[p.X+1][p.Y], chessboard[p.X+2][p.Y], chessboard[p.X][p.Y] = "X", "X", "X"
+		}
+	}
+
+	if p.Y < len(chessboard[0])-2 {
+		a, b, c := chessboard[p.X][p.Y+1], chessboard[p.X][p.Y+2], chessboard[p.X][p.Y]
+		if a == b && b == c {
+			bol = true
+			fmt.Println("Y消除 ------")
+			chessboard[p.X][p.Y+1], chessboard[p.X][p.Y+2], chessboard[p.X][p.Y] = "X", "X", "X"
+		}
+	}
+	return bol
 }
 
 func initChessboard(chessboard [][]string) {
@@ -60,25 +116,6 @@ func initChessboard(chessboard [][]string) {
 				// ii = *i
 				chess[index] = element[i]
 			}
-		}
-	}
-}
-
-// ScanPos is 扫描棋盘
-func ScanPos(p *point) {
-	if p.X < len(chessboard)-2 {
-		a, b, c := chessboard[p.X+1][p.Y], chessboard[p.X+2][p.Y], chessboard[p.X][p.Y]
-		if a == b && b == c {
-			fmt.Println("X消除 ------")
-			chessboard[p.X+1][p.Y], chessboard[p.X+2][p.Y], chessboard[p.X][p.Y] = "X", "X", "X"
-		}
-	}
-
-	if p.Y < len(chessboard[0])-2 {
-		a, b, c := chessboard[p.X][p.Y+1], chessboard[p.X][p.Y+2], chessboard[p.X][p.Y]
-		if a == b && b == c {
-			fmt.Println("Y消除 ------")
-			chessboard[p.X][p.Y+1], chessboard[p.X][p.Y+2], chessboard[p.X][p.Y] = "X", "X", "X"
 		}
 	}
 }
