bd718e2a018dc55880aa69646395234f3750a8aa
20190046 <494608448@qq.com>
2019-05-15 18:55:41 +0800

    backup

--- algorithm/three-elimination2/main.go
+++ algorithm/three-elimination2/main.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
-	"strconv"
-	"strings"
 )
 
 var chessboard [][]string
 var element []string
-
-//var targetPoint map[point]point
-
 var targetPoint map[point]map[point][]point
-
-// {1,1,*}:{1,2,}:{}
 
 type point struct {
 	X       int
@@ -24,44 +16,47 @@ type point struct {
 }
 
 func main() {
+	chessDrop()
+	// checkPos()
 
-	checkPos()
+	// for c := range chessboard {
+	// 	fmt.Println(chessboard[c])
+	// }
 
-	for c := range chessboard {
-		fmt.Println(chessboard[c])
-	}
+	// onTrigger()
 
-	onTrigger()
+	// for {
+	// 	var p1, p2 string
+	// 	fmt.Println("请输入要移动的棋子的位置 与 移动后的位置: eg:(1,1 1,2)")
+	// 	fmt.Scanln(&p1, &p2)
+	// 	X1, _ := strconv.ParseFloat(strings.Split(p1, ",")[0], 64)
+	// 	Y1, _ := strconv.ParseFloat(strings.Split(p1, ",")[1], 64)
 
-	for {
-		var p1, p2 string
-		fmt.Println("请输入要移动的棋子的位置 与 移动后的位置: eg:(1,1 1,2)")
-		fmt.Scanln(&p1, &p2)
-		X1, _ := strconv.ParseFloat(strings.Split(p1, ",")[0], 64)
-		Y1, _ := strconv.ParseFloat(strings.Split(p1, ",")[1], 64)
+	// 	X2, _ := strconv.ParseFloat(strings.Split(p2, ",")[0], 64)
+	// 	Y2, _ := strconv.ParseFloat(strings.Split(p2, ",")[1], 64)
 
-		X2, _ := strconv.ParseFloat(strings.Split(p2, ",")[0], 64)
-		Y2, _ := strconv.ParseFloat(strings.Split(p2, ",")[1], 64)
+	// 	if math.Abs(X2-X1) > 1 || math.Abs(Y2-Y1) > 1 {
+	// 		fmt.Println("请输入正确的位置")
+	// 	} else {
+	// 		p1 := point{int(X1), int(Y1), chessboard[int(X1)][int(Y1)]}
+	// 		p2 := point{int(X2), int(Y2), chessboard[int(X2)][int(Y2)]}
+	// 		tp1 := targetPoint[p1][p2]
+	// 		tp2 := targetPoint[p2][p1] //TODO 优化
+	// 		fmt.Println("tp1", tp1, "tp2", tp2)
 
-		if math.Abs(X2-X1) > 1 || math.Abs(Y2-Y1) > 1 {
-			fmt.Println("请输入正确的位置")
-		} else {
-			p1 := point{int(X1), int(Y1), chessboard[int(X1)][int(Y1)]}
-			p2 := point{int(X2), int(Y2), chessboard[int(X2)][int(Y2)]}
-			tp := targetPoint[p1][p2]
-			fmt.Println("tp", tp)
+	// 		if len(tp1) != 0 || len(tp2) != 0 {
 
-			//chessboard[int(X1)][int(Y1)], chessboard[int(X2)][int(Y2)] = chessboard[int(X2)][int(Y2)], chessboard[int(X1)][int(Y1)]
-			if len(tp) != 0 {
-				fmt.Println("可以消除")
-			}
-		}
+	// 			chessboard[int(X1)][int(Y1)], chessboard[int(X2)][int(Y2)] = chessboard[int(X2)][int(Y2)], chessboard[int(X1)][int(Y1)]
 
-		fmt.Println(" 消除后的棋盘 ")
-		for c := range chessboard {
-			fmt.Println(chessboard[c])
-		}
-	}
+	// 			checkPos()
+	// 			fmt.Println(" 消除后的棋盘 ")
+	// 			for c := range chessboard {
+	// 				fmt.Println(chessboard[c])
+	// 			}
+	// 			onTrigger()
+	// 		}
+	// 	}
+	// }
 }
 
 func init() {
@@ -125,6 +120,34 @@ func onTrigger() {
 	fmt.Println("targetPoint", targetPoint)
 }
 
+func chessDrop() {
+	// TODO: 下落队列
+
+	chessboard[6][6] = "X"
+
+	dropQueue := []point
+
+	for row := len(chessboard) - 2; row >= 0; row-- {
+		for col := len(chessboard[row]) - 1; col >= 0; col-- {
+
+			if chessboard[row+1][col] == "X" {
+				chessboard[row+1][col] = chessboard[row][col]
+				chessboard[row][col] = "X"
+			}
+
+			append(dropQueue,point{})
+			 
+			//fmt.Println("chessboard[col]", chessboard[row][col])
+			// chessboard[0][0] = chessboard[1][0]
+		}
+	}
+
+	fmt.Println()
+	for c := range chessboard {
+		fmt.Println(chessboard[c])
+	}
+}
+
 func checkPos() {
 loop:
 	for row, cb := range chessboard {
@@ -146,6 +169,7 @@ loop:
 					}
 
 					initChessboard(chessboard)
+
 					goto loop
 				}
 			}
@@ -156,7 +180,7 @@ loop:
 					fmt.Println("消除 Y", a, b, c)
 					chessboard[p.X][p.Y+1], chessboard[p.X][p.Y+2], chessboard[p.X][p.Y] = "X", "X", "X"
 
-					for i := p.Y + 2; i < len(chessboard[0]); i++ {
+					for i := p.Y + 3; i < len(chessboard[0]); i++ {
 						if chessboard[p.X][i] == a {
 							fmt.Println("继续消除 Y", chessboard[p.X][i], p.X, i)
 							chessboard[p.X][i] = "X"
@@ -179,53 +203,57 @@ func searchXTarget(f, a, b, c point) {
 	case a.element:
 		if f.X != 0 {
 			if chessboard[f.X-1][f.Y] == b.element {
-				targetPoint[f] = point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}
-				targetPoint2[f] = make(map[point][]point)
-				targetPoint2[f][point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}] = []point{b, c}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}] = []point{b, c}
 			}
 		}
 
 		if f.Y != len(chessboard)-1 {
 			if chessboard[f.X][f.Y+1] == b.element {
-				targetPoint[f] = point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}
-				targetPoint2[f] = make(map[point][]point)
-				targetPoint2[f][point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}] = []point{b, c}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}] = []point{b, c}
 			}
 		}
 
 		if f.Y != 0 {
 			if chessboard[f.X][f.Y-1] == b.element {
-				targetPoint[f] = point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}] = []point{b, c}
 			}
 		}
 	case b.element:
 		if f.Y != len(chessboard)-1 {
 			if chessboard[f.X][f.Y+1] == a.element {
-				targetPoint[f] = point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}] = []point{a, c}
 			}
 		}
 
 		if f.Y != 0 {
 			if chessboard[f.X][f.Y-1] == a.element {
-				targetPoint[f] = point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}] = []point{a, c}
 			}
 		}
 	case c.element:
 		if f.X != len(chessboard)-1 {
 			if chessboard[f.X+1][f.Y] == a.element {
-				targetPoint[f] = point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}] = []point{a, b}
 			}
 		}
 
 		if f.Y != len(chessboard)-1 {
 			if chessboard[f.X][f.Y+1] == a.element {
-				targetPoint[f] = point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}] = []point{a, b}
 			}
 		}
 
 		if f.Y != 0 {
 			if chessboard[f.X][f.Y-1] == a.element {
-				targetPoint[f] = point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}] = []point{a, b}
 			}
 		}
 	}
@@ -236,49 +264,57 @@ func searchYTarget(f, a, b, c point) {
 	case a.element:
 		if f.Y != 0 {
 			if chessboard[f.X][f.Y-1] == b.element {
-				targetPoint[f] = point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y - 1, chessboard[f.X][f.Y-1]}] = []point{b, c}
 			}
 		}
 
 		if f.X != 0 {
 			if chessboard[f.X-1][f.Y] == b.element {
-				targetPoint[f] = point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}] = []point{b, c}
 			}
 		}
 
 		if f.X != len(chessboard)-1 {
 			if chessboard[f.X+1][f.Y] == b.element {
-				targetPoint[f] = point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}] = []point{b, c}
 			}
 		}
 	case b.element:
 		if f.X != 0 {
 			if chessboard[f.X-1][f.Y] == a.element {
-				targetPoint[f] = point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}] = []point{a, c}
 			}
 		}
 
 		if f.X != len(chessboard)-1 {
 			if chessboard[f.X+1][f.Y] == a.element {
-				targetPoint[f] = point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}] = []point{a, c}
 			}
 		}
 	case c.element:
 		if f.Y != len(chessboard)-1 {
 			if chessboard[f.X][f.Y+1] == b.element {
-				targetPoint[f] = point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X, f.Y + 1, chessboard[f.X][f.Y+1]}] = []point{a, b}
 			}
 		}
 
 		if f.X != 0 {
 			if chessboard[f.X-1][f.Y] == b.element {
-				targetPoint[f] = point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X - 1, f.Y, chessboard[f.X-1][f.Y]}] = []point{a, b}
 			}
 		}
 
 		if f.X != len(chessboard)-1 {
 			if chessboard[f.X+1][f.Y] == b.element {
-				targetPoint[f] = point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}
+				targetPoint[f] = make(map[point][]point)
+				targetPoint[f][point{f.X + 1, f.Y, chessboard[f.X+1][f.Y]}] = []point{a, b}
 			}
 		}
 	}
@@ -288,7 +324,7 @@ func initChessboard(chessboard [][]string) {
 	for _, chess := range chessboard {
 		for index, c := range chess {
 			if c == "X" {
-				i := rand.Intn(4)
+				i := rand.Intn(5)
 				chess[index] = element[i]
 			}
 		}
